0c65598d062627ad01499b848018f170c84ee13b
Pofi Abel <peterabel07@gmail.com>
2021-08-22 23:19:42 +0100

    in progress

--- phonebook.c
+++ phonebook.c
@@ -2,19 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct person
+{
+    /* data */
+    string name;
+    string number;
+} person;
+
+const int NUMBER = 10;
+
 int main (void)
 {
-    string names[] = {"Brian", "David"};
-    string numbers[] = {"+1-167-495-1000", "+1-949-468-2750"};
+    person people[NUMBER];
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < NUMBER; i++)
     {
-        if(strcmp(names[i], "David") == 0)
-        {
-            printf("Found %s\n", numbers[i]);
-            return 0;
-        }
+        people[i].name = get_string("What's the name? ");
+        people[i].number = get_string("What's their number ");
     }
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (strcmp(people[i].name, "David") == 0)
+            {
+                printf("Found %s\n", people[i].number);
+                return 0;
+            }
+        }
     printf("Not found\n");
     return 1;
 }
